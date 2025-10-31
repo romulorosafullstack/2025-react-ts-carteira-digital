c7101993e4ce57e83c7ccacdd74ee3b88eed38bd
Romulo Rosa | Front-End <romulorosafullstack@gmail.com>
2025-10-31 17:00:26 -0300

    styles theme dark

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,18 @@
+export default {
+  title: 'dark',
+  color: {
+    primary: '#1B1F38',
+    secondary: '#252A48',
+    tertiary: '#313862',
+
+    white: '#ffffff',
+    black: '#000000',
+    gray: '#9e9e9e',
+
+    success: '#4E41F0',
+    error: '#E44C4E',
+    info: '#F7931B',
+
+    text: 'white',
+  }
+} 
