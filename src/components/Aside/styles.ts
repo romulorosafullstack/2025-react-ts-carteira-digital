e089b495d6d35e5704ae9079b891e28f12dd1b2d
Romulo Rosa | Front-End <romulorosafullstack@gmail.com>
2025-10-31 16:42:21 -0300

    acertando o grid

--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
+export const Container = styled.section`
+  grid-area: AS;
+  border: 2px solid green;
 `;
--- src/components/MainHeader/styles.ts
+++ src/components/MainHeader/styles.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  width: 100vw;
-  height: 100vh;
+export const Container = styled.section`
+  grid-area: MH;
+  border: 2px solid blue;
 `;
