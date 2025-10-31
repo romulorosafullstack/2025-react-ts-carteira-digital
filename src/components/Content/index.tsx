e2620b03bc5bb5f60785fc0d0f7875133784fee9
Romulo Rosa | Front-End <romulorosafullstack@gmail.com>
2025-10-31 01:46:28 -0300

    component Content

---
+++ src/components/Content/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles"
+
+export const Content: React.FC = () => {
+  return (
+    <>
+      <Container>
+        <h1>Content</h1>
+      </Container>
+    </>
+  )
+}
