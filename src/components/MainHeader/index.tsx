de158c20883fdafa2ab5c1773a01b582a580c3c0
Romulo Rosa | Front-End <romulorosafullstack@gmail.com>
2025-10-31 01:46:14 -0300

    component MainHeader

---
+++ src/components/MainHeader/index.tsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles"
+
+export const MainHeader: React.FC = () => {
+  return (
+    <>
+      <Container>
+        <h1>MainHeader</h1>
+      </Container>
+    </>
+  )
+}
