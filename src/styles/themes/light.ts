3368f155a2e8dc6694450765ec48ca6806e742f8
Romulo Rosa | Front-End <romulorosafullstack@gmail.com>
2025-10-31 17:05:58 -0300

    styles theme light

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,18 @@
+export default {
+  title: 'light',
+  color: {
+    primary: '#DCDCDC',
+    secondary: '#FFFFFF',
+    tertiary: '#F5F5F5',
+
+    white: '#ffffff',
+    black: '#000000',
+    gray: '#9e9e9e',
+
+    success: '#03BB85',
+    error: '#FF6961',
+    info: '#3B5998',
+
+    text: 'black',
+  }
+} 
