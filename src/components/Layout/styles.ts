accf5d88966f3e0862c55bc200adc087b0977a55
Romulo Rosa | Front-End <romulorosafullstack@gmail.com>
2025-10-31 17:07:27 -0300

    grig alteracao largura coluna aside

--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Grid = styled.div`
   display: grid;
-  grid-template-columns: 250px auto;
+  grid-template-columns: 310px auto;
   grid-template-rows: 70px auto;
   grid-template-areas:
   'AS MH'
