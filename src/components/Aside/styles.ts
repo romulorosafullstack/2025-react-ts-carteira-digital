3604f0d119b4ad6496d0691bc000567e9a48d906
Romulo Rosa | Front-End <romulorosafullstack@gmail.com>
2025-11-03 13:17:29 -0300

    Aside corrigindo a borda

--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -4,5 +4,5 @@ export const Container = styled.section`
   grid-area: AS;
   background-color: ${props => props.theme.colors.secondary};
   color: ${props => props.theme.colors.text};
-  border: 0.1rem solid ${props => props.theme.colors.white};
+  border-right: 0.1rem solid ${props => props.theme.colors.white};
 `;
